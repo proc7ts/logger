6f9d6c433d6ab394be1d35a30be36982635cfe77
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-06-14 20:17:54 +0700

    Add `logDefer()` loggable builder

---
+++ src/loggable/log-defer.ts
@@ -0,0 +1,27 @@
+import { DueLog } from './due-log';
+import { Loggable } from './loggable';
+
+/**
+ * Creates a loggable value processed when the message is actually logged.
+ *
+ * The value will be {@link Loggable.toLog expanded} at `out` {@link DueLog.on logging stage}.
+ *
+ * @param toLog - Performs additional message processing before it is logged. It will be called by
+ * {@link @Loggable.toLog} method implementation at appropriate logging stage.
+ *
+ * @returns New loggable value.
+ */
+export function logDefer<TTarget extends DueLog.Target>(
+    toLog: (this: void, target: TTarget) => unknown | void,
+): Loggable<TTarget> {
+  return {
+    toLog(target) {
+
+      const { on = 'out' } = target;
+
+      if (on === 'out') {
+        return toLog(target);
+      }
+    },
+  };
+}
