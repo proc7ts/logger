39886b9907402f917760ca9d39f99d32d204fe53
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-09-16 12:17:51 +0700

    Drop dependency on `@proc7ts/context-values`

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,3 @@
-import { CxEntry, CxGlobals, cxRecent, cxScoped } from '@proc7ts/context-values';
-import { consoleLogger, proxyLogger } from './loggers/mod.js';
-
 /**
  * Basic logger interface.
  */
@@ -42,24 +39,3 @@ export interface Logger {
    */
   trace(...args: unknown[]): void;
 }
-
-/**
- * Global context entry containing logger to use.
- *
- * Logs to {@link consoleLogger console} by default.
- */
-export const Logger: CxEntry<Logger> = {
-  perContext: /*#__PURE__*/ cxScoped(
-    CxGlobals,
-    /*#__PURE__*/ cxRecent({
-      create: (recent, _) => recent,
-      byDefault: _ => consoleLogger,
-      assign({ get, to }) {
-        const logger = proxyLogger(get);
-
-        return receiver => to((_, by) => receiver(logger, by));
-      },
-    }),
-  ),
-  toString: () => '[Logger]',
-};
